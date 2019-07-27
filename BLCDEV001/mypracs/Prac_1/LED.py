02293856d8698191858864a0e5867efa7b10bbfe
dVeon-loch <blochdevon@gmail.com>
2019-07-27 22:20:27 +0100

    Renamed LED program and added pushbutton interrupt functionality for 1 LED

--- BLCDEV001/mypracs/Prac_1/LED.py
+++ BLCDEV001/mypracs/Prac_1/LED.py
@@ -14,14 +14,22 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def toggle_on(channel):
+	GPIO.output(17, 1)
+def toggle_off(channel):
+	GPIO.output(17,0)
+GPIO.add_event_detect(18,GPIO.RISING, callback=toggle_on, bouncetime=300)
+
+	
 # Logic that you write
 def main():
- 	for i in range(0,10):	
-		GPIO.output(17, GPIO.HIGH)
-		time.sleep(0.5)
-		GPIO.output(17, GPIO.LOW)
-		time.sleep(0.5)
-	GPIO.cleanup()		
+	state = 0
+	
+ 	message = input("Press enter to quit\n\n") # Run until someone presses enter
+	GPIO.cleanup() # Clean up		
 
 
 # Only run the functions if 
@@ -38,3 +46,5 @@ if __name__ == "__main__":
         GPIO.cleanup()
         print("Some other error occurred")
         print(e.message)
+    finally:
+	GPIO.cleanup()	
