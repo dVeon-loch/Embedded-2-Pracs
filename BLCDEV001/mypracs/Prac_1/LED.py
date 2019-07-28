2a705b9ccbabb15fce02dd171a4f81d391108a16
dVeon-loch <blochdevon@gmail.com>
2019-07-28 23:37:18 +0100

    Added full 3-bit Binary counter code with interrupts for increment/decrement and wrapping

--- BLCDEV001/mypracs/Prac_1/LED.py
+++ BLCDEV001/mypracs/Prac_1/LED.py
@@ -14,15 +14,36 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
 
 
-def toggle_on(channel):
-	GPIO.output(17, 1)
-def toggle_off(channel):
-	GPIO.output(17,0)
-GPIO.add_event_detect(18,GPIO.RISING, callback=toggle_on, bouncetime=300)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+number = 0
 
+def increment(channel):
+	global number
+	number = (0 if number == 7 else (number+1))
+	print("Decimal Number: "+str(number))
+	bin_arr = list(str(bin(number)))
+	print("Binary Number: "+str(bin_arr))
+	GPIO.output(17,int(bin_arr[-1]))
+	GPIO.output(27,int(0 if bin_arr[-2]== 'b' else bin_arr[-2]))
+	GPIO.output(22,int(0 if bin_arr[-3]== 'b' else bin_arr[-3]))
+def decrement(channel):
+	global number
+	
+	number = 7 if number == 0 else number-1
+	print("Decimal Number: "+str(number))
+        bin_arr = list(str(bin(number)))
+	print("Binary Number: "+str(bin_arr))
+        GPIO.output(17,int(bin_arr[-1]))
+        GPIO.output(27,int(0 if bin_arr[-2]== 'b' else bin_arr[-2]))
+        GPIO.output(22,int(0 if bin_arr[-3]== 'b' else bin_arr[-3]))
+	
+GPIO.add_event_detect(23,GPIO.RISING, callback=increment, bouncetime=300)
+GPIO.add_event_detect(24,GPIO.RISING, callback=decrement, bouncetime=300)
 	
 # Logic that you write
 def main():
